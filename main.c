11b1d3f83cfecb4abdda2e6f8458e93ea70136b8
VladMikheda <vladmikheda@gmail.com>
2021-10-12 00:08:31 +0200

    new health check

--- main.c
+++ main.c
@@ -10,19 +10,27 @@ int main(int argc, char** argv){
     FILE* file_name;
     file_name = fopen( argv[1],"r");
     printf("%s\n",argv[1]);
-    t_token token;
-
+    t_token* token;
+    token = malloc(sizeof (t_token));
+    token->lexeme = malloc((sizeof (t_lexeme)));
+    token->lexeme->inter = malloc(sizeof (t_str));
+    string_init(token->lexeme->inter);
     file_ptr(file_name);
-    get_token(&token);
-    printf("%d\n",token.token_name);
-    get_token(&token);
-    printf("%d\n",token.token_name);
-    get_token(&token);
-    printf("%d\n",token.token_name);
-    get_token(&token);
-    printf("%d\n",token.token_name);
-    get_token(&token);
-    printf("%d\n",token.token_name);
+
+    get_token(token);
+    printf("%d\n",token->token_name);
+    get_token(token);
+    printf("%d\n",token->token_name);
+    get_token(token);
+    printf("%d\n",token->token_name);
+    get_token(token);
+    printf("%d\n",token->token_name);
+    get_token(token);
+    printf("%d\n",token->token_name);
+
+    string_free(token->lexeme->inter);
+    free(token->lexeme);
+    free(token);
 
     return 0;
 }
