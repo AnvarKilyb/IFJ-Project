1f0a4cb2edf4604ca606a3b38835f1bedccf3c2f
VladMikheda <vladmikheda@gmail.com>
2021-10-19 03:12:32 +0200

    add check stack

--- main.c
+++ main.c
@@ -5,8 +5,14 @@
 
 
 #include "lexicalanalysis.c"
+#include "mystack.c"
 
 int main(int argc, char** argv){
+/*
+ *
+ * Проверка лексического анализатора
+ *
+ */
     //TODO обработка ошибок с читением имени файла
     FILE* file_name;
     file_name = fopen( argv[1],"r");
@@ -43,6 +49,34 @@ int main(int argc, char** argv){
     string_free(token->lexeme->inter);
     free(token->lexeme);
     free(token);
+
+
+/*
+ *
+ * Проверка стека
+ *
+ */
+    t_stack stack;
+    stack_init(&stack);
+    stack_push(&stack,10);
+    printf("%d......%d\n",stack.stack_top->data,stack.amount_of_elements);
+    stack_push(&stack,11);
+    printf("%d......%d\n",stack.stack_top->data,stack.amount_of_elements);
+    stack_push(&stack,12);
+    printf("%d......%d\n",stack.stack_top->data,stack.amount_of_elements);
+    int a = stack_top(&stack);
+    printf("%d\n",a);
+    stack_pop(&stack);
+    printf("%d......%d\n",stack.stack_top->data,stack.amount_of_elements);
+    a = stack_top(&stack);
+    stack_pop(&stack);
+    printf("%d\n",a);
+    printf("%d......%d\n",stack.stack_top->data,stack.amount_of_elements);
+    a = stack_top(&stack);
+    stack_pop(&stack);
+    printf("%d\n",a);
+    stack_pop(&stack);
+    printf("%s......%d\n",stack.stack_top,stack.amount_of_elements);
 
     return 0;
 }
