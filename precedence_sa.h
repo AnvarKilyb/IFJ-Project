97884cfaa1a09c00ecf22dcaff2677f213da952f
Anvar Kilybayev <47081931+AnvarKilyb@users.noreply.github.com>
2021-12-05 20:47:09 +0100

    Update precedence_sa.h

--- precedence_sa.h
+++ precedence_sa.h
@@ -83,5 +83,5 @@ void delete_ast(AST_leaf *leaf);
 int find_operation(t_token *token, t_stack *stack);
 AST_leaf *reduce_by_rule(t_stack *tmp_stack, t_stack *stack, AST_leaf *tree, e_error_message *e_check);
 AST_leaf *get_expression(t_token *token, t_stack *stack, AST_leaf *tree, e_error_message *e_check);
-AST_leaf *precede_expression(t_token *token,t_ast_node *ast_node, e_error_message *e_check);
+AST_leaf *precede_expression(t_token *token, e_error_message *e_check);
 #endif
